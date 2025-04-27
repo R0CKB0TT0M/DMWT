84f700b2a37cc712f6e66316871d367c7a335197
David Hack <davidusw9@gmail.com>
2025-04-28 01:45:59 +0200

    test

--- src/app/body/comments/page.js
+++ src/app/body/comments/page.js
@@ -2,6 +2,8 @@
 import { neon } from '@neondatabase/serverless';
 
 export default function Page() {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    let comment = sql.query("SELECT * FROM comments;"); 
   async function create(formData) {
     'use server';
     // Connect to the Neon database
@@ -10,17 +12,19 @@ export default function Page() {
     // Insert the comment from the form into the Postgres database
     //await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
     //sql'INSERT INTO comments (comment) VALUES ($1)'
-    await sql.query("INSERT INTO comments (comment) VALUES ($1)",[comment])
-    console.log(sql)
-    let comments = await sql.query("Select * FROM comments")
-    let commentstr = comments.toString()
+    await sql.query("INSERT INTO comments (comment) VALUES ($1)",[comment]);
+    console.log(sql);
   }
+  
+
+
+
   return (<div>
     <form action={create}>
       <input type="text" placeholder="write a comment" name="comment" />
       <button type="submit">Submit</button>
     </form>
-    <p id="comments">{commentstr}</p>
+    <p id="comments">{comment}</p>
     </div>
 );
     }
