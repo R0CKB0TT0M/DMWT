5649ba58573460ce90b30fb20b80e3baf2572ead
David Hack <davidusw9@gmail.com>
2025-04-28 00:27:11 +0200

    fix sql statement

--- src/app/body/comments/page.js
+++ src/app/body/comments/page.js
@@ -8,7 +8,9 @@ export default function Page() {
     const sql = neon(`${process.env.DATABASE_URL}`);
     const comment = formData.get('comment');
     // Insert the comment from the form into the Postgres database
-    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+    //await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+    //sql'INSERT INTO comments (comment) VALUES ($1)'
+    sql.query("INSERT INTO comments (comment) VALUES ($1)",[comment])
   }
 
   return (
