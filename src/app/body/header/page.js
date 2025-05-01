c9e77ef34f41752980afce8e8d219826cca1510d
DanielEck1 <167995538+DanielEck1@users.noreply.github.com>
2025-05-01 20:25:21 +0200

    Header mit 2 buttons ( zu FAQ und login)

--- src/app/body/header/page.js
+++ src/app/body/header/page.js
@@ -2,20 +2,15 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        // rechtsbündig, vertikal, blau, schrift weiß, Abstand 4
-        <header className="align= right flex justify-end items-center bg-blue-600 text-white p-8">
-            <div className="flex space-x-8">
-                {/*Button 1*/}
+        <header className="bg-blue-600 py-4">
+            <div className="container mx-auto flex justify-end space-x-4">
                 <Link href="/FAQ">
-                    {/*hintergrund grün, weißer text, abstand, button abgerundet, grauer hintergrund(beim hover), transition beim Übergang*/}
-                    <button className="align=right bg-green text-white py-2 px-4 rounded hover:bg-gray-200 transition">
+                    <button className="bg-green-600 text-white font-semibold py-2 px-4 rounded hover:bg-gray-200 transition">
                         Ask a Question?
                     </button>
                 </Link>
-                    {/* Button 2*/}
                 <Link href="/login">
-                    {/*hintergrund blau, weißer text, button abgerundet, grauer hintergrund(beim hover), transition beim Übergang*/}
-                    <button className="align=right bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-gray-200 transition">
+                    <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-gray-200 transition">
                         Login
                     </button>
                 </Link>
